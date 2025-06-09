caf24234167d4aaf1c67ec7d83469735b34ce24c
Tuhin Mandal <73009536+MandalTuhin@users.noreply.github.com>
2025-06-09 16:02:37 +0530

    finished exercise of alert-prompt-confirm on JSinfo

--- alert.js
+++ alert.js
@@ -1,8 +1,3 @@
+let userName = prompt("Enter your name");
 
-let isUserLoggedInOther= confirm("Are you logged in on other devices?");
-
-alert(isUserLoggedInOther);
-
-// let age = prompt('How old are you?', 100);
-//
-// alert(`You are ${age} years old!`); // You are 100 years old!
+alert(`Hello there!, ${userName}`);
