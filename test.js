7042ae182e471f65cef9e199f85e5897832b5946
Tuhin Mandal <73009536+MandalTuhin@users.noreply.github.com>
2025-06-09 14:32:05 +0530

    experimenting on JS string data type.

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
-console.log(NaN);
+let userName = "Tuhin Mandal";
+let planetName = "Earth";
+
+console.log(`Hello Mr. ${userName}, You are gladly welcome on planet ${planetName}`);
+console.log(`The result of 2 + 3 is ${2 + 3}`);
+console.log("The result of 2 + 3 is ${2 + 3}");
 
