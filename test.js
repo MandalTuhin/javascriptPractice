6f0d60d463c98e25297d4b2f6d5f38b330f03b4b
Tuhin Mandal <73009536+MandalTuhin@users.noreply.github.com>
2025-06-14 12:20:05 +0530

    tested type conversion to number.

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 // type conversions;
-// 1. to string conversion;
+// 2. Integer converison;
+// Integer conversion in javascript is implicit.
+// eg. :-
+//
+let var1;
+let var2 = Number("this is an arbitary string");
 
+// now typeof var1 should return NaN;
 
-let value = true;
+console.log(var2);
 
-console.log(typeof value); // will O/P : "boolean";
-
-value = String(value); // now value stores the string "true";
-
-console.log(typeof value); // should show "string";
