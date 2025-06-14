4cf463948db23cd6cfca31201a820bf4836cd276
Tuhin Mandal <73009536+MandalTuhin@users.noreply.github.com>
2025-06-14 12:32:29 +0530

    tested comparision operators and weird behaviour of null data type

--- test.js
+++ test.js
@@ -1,12 +1,21 @@
-// type conversions;
-// 2. Integer converison;
-// Integer conversion in javascript is implicit.
-// eg. :-
-//
-let var1;
-let var2 = Number("this is an arbitary string");
+let a = 0;
+console.log(Boolean(a));
 
-// now typeof var1 should return NaN;
+let b = '0';
+console.log(Boolean(b));
 
-console.log(var2);
+console.log(a === b);
 
+console.log(0 === false);
+
+console.log(null == undefined);
+console.log(null === undefined);
+
+console.log(null > 0); // false; null is converted to zero and 0 > zero is false;
+console.log(null == 0) // false; null and undefined are equal in == and are not equal to anything else;
+
+console.log(null >= 0) // true; here null is converted into number and zero greater than zero is true;
+
+console.log(null >= ''); // should retrun true;
+
+console.log(null == ""); // should return false;
